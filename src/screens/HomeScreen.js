4727b318d71b9189e231100bf99639017e79e660
Vivaswan Shetty <vivashetty2006@gmail.com>
2026-09-15 16:18:45 +0530

    fix: import useMemo in HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from "react";
+import React, { useState, useEffect, useCallback, useRef, useMemo } from "react";
 import {
     View, Text, ScrollView, TouchableOpacity, StyleSheet,
     Dimensions, StatusBar, Animated, ImageBackground, Image, Modal, Share, Easing,
